180aa3f591cf22b1ac6f68308a5051434a0c8d47
Hype AMD <muhammadtino002@gmail.com>
2026-04-09 09:47:23 +0700

    initial commit

---
+++ app/src/main/java/com/example/greatingcard/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.greatingcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.greatingcard.ui.theme.GreatingCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GreatingCardTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Muhammad Tino Cahyo Desta",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Box (modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White)){
+        Text(
+            text = "Hello $name! How Are You?",
+            fontSize = 20.sp,
+            modifier = Modifier.padding(50.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GreatingCardTheme {
+        Greeting("Muhammad Tino Cahyo Desta")
+    }
+}
